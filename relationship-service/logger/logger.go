d27e1f7ca6ab4a76b8c24db3420fa0200029239a
dusancina98 <dule_srb@hotmail.rs>
2021-06-09 22:26:47 +0200

    Implement relationship-service logging mechanism

---
+++ relationship-service/logger/logger.go
@@ -0,0 +1,26 @@
+package logger
+
+import (
+	"github.com/sirupsen/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+var Logger *logrus.Logger
+var LoggingEntry *logrus.Entry
+
+func InitLogger()  {
+	Logger = logrus.New()
+
+	Logger.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	Logger.SetReportCaller(true)
+
+	Logger.SetOutput(&lumberjack.Logger{
+		Filename:   "./logger/logs/relationship-service.log",
+		MaxSize:    1,  // megabytes after which new file is created
+		MaxBackups: 3,  // number of backups
+		MaxAge:     30, //days
+	})
+}
+
--- relationship-service/main.go
+++ relationship-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
 	"os"
 	"flag"
 	"github.com/labstack/echo"
@@ -9,11 +10,15 @@ import (
 	"relationship-service/controller/middleware"
 	"relationship-service/controller/router"
 	"relationship-service/interactor"
+	"relationship-service/logger"
 )
 
 var runServer = flag.Bool("relationship-service", os.Getenv("IS_PRODUCTION") == "true", "production is -server option require")
 
 func main(){
+
+	logger.InitLogger()
+
 	conf.NewConfig(*runServer)
 	driver, err := neo4j.NewDriver(conf.Current.Database.Host, neo4j.BasicAuth(conf.Current.Database.User, conf.Current.Database.Password, ""))
 	if err != nil {
@@ -25,6 +30,11 @@ func main(){
 
 	router.NewRouter(e, h)
 	middleware.NewMiddleware(e)
+
+	logger.Logger.WithFields(logrus.Fields{
+		"host": conf.Current.Server.Host,
+		"port":   conf.Current.Server.Port,
+	}).Info("Server started")
 
 	if os.Getenv("IS_PRODUCTION") == "true" {
 		e.Start(":"+ conf.Current.Server.Port)
